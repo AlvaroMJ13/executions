848871226f337d34c778c2b3d9f82f0910e314ee
Alvaro Mateos <alvaro.mateos@serquo.com>
2022-08-23 10:39:57 +0200

    ADD first test

--- src/test/java/com/payment/executions/ExecutionsApplicationTests.java
+++ src/test/java/com/payment/executions/ExecutionsApplicationTests.java
@@ -1,13 +1,37 @@
 package com.payment.executions;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import com.payment.executions.controller.response.ExecutionResponse;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
 class ExecutionsApplicationTests {
 
+	@Autowired
+    private WebTestClient webTestClient;
+	
 	@Test
 	void contextLoads() {
+
+
+		webTestClient
+		.get()
+		.uri("/executions/5da099c1-5548-4d26-9cf7-d726633b62d3")
+		.exchange()
+		.expectStatus()
+		.is2xxSuccessful()
+		.expectBody(ExecutionResponse.class)
+		.consumeWith(entityExchangeResult -> {
+			ExecutionResponse response = entityExchangeResult.getResponseBody();
+			assert response.getId().toString().equals("5da099c1-5548-4d26-9cf7-d726633b62d3");
+
+		});
+
 	}
 
 }

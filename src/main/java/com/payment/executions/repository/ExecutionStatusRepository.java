a446c78631489a33cc9b5114e327ea5fbad2a946
Alvaro Mateos <alvaro.mateos@serquo.com>
2022-08-23 14:05:13 +0200

    Improve tests

--- src/main/java/com/payment/executions/repository/ExecutionStatusRepository.java
+++ src/main/java/com/payment/executions/repository/ExecutionStatusRepository.java
@@ -1,13 +1,12 @@
 package com.payment.executions.repository;
 
-import java.util.UUID;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.payment.executions.dao.ExecutionStatusDAO;
+import com.payment.executions.dao.ExecutionStatusId;
 
 @Repository
-public interface ExecutionStatusRepository extends JpaRepository<ExecutionStatusDAO, UUID> {
+public interface ExecutionStatusRepository extends JpaRepository<ExecutionStatusDAO, ExecutionStatusId> {
 	
 }
--- src/test/java/com/payment/executions/ExecutionsApplicationTests.java
+++ src/test/java/com/payment/executions/ExecutionsApplicationTests.java
@@ -1,34 +1,67 @@
 package com.payment.executions;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import com.payment.executions.controller.response.ExecutionResponse;
+import com.payment.executions.dao.ExecutionDAO;
+import com.payment.executions.dao.ExecutionStatusDAO;
+import com.payment.executions.dao.ExecutionStatusId;
+import com.payment.executions.repository.ExecutionRepository;
+import com.payment.executions.repository.ExecutionStatusRepository;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
+@TestInstance(Lifecycle.PER_CLASS)
 class ExecutionsApplicationTests {
 
 	@Autowired
     private WebTestClient webTestClient;
 	
+	@Autowired
+	private ExecutionRepository executionRepository;
+	
+	@Autowired
+	private ExecutionStatusRepository executionStatusRepository;
+	
+	private UUID id;
+	
+	@BeforeAll
+	void init () {
+		id = executionRepository.save(ExecutionDAO.builder().entityId(1).gtsMessageId(1313).build()).getId();
+		executionStatusRepository.save(ExecutionStatusDAO.builder().id(id).statusId(1).timestamp(LocalDateTime.now()).build());
+	}
+	
+	@AfterAll
+	void finish () {
+		executionStatusRepository.deleteById(ExecutionStatusId.builder().id(id).statusId(1).build());
+		executionRepository.deleteById(id);
+	}
+	
 	@Test
-	void contextLoads() {
+	void getById() {
 
 
 		webTestClient
 		.get()
-		.uri("/executions/5da099c1-5548-4d26-9cf7-d726633b62d3")
+		.uri("/executions/" + id )
 		.exchange()
 		.expectStatus()
 		.is2xxSuccessful()
 		.expectBody(ExecutionResponse.class)
 		.consumeWith(entityExchangeResult -> {
 			ExecutionResponse response = entityExchangeResult.getResponseBody();
-			assert response.getId().toString().equals("5da099c1-5548-4d26-9cf7-d726633b62d3");
+			assert response.getId().equals(id);
 
 		});
 
